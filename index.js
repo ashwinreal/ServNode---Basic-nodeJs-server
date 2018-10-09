8245fb91b8a4aa24f241564260af92eb2fce689c
Ashwin Sreekumar (assreeku) <assreeku@cisco.com>
2018-10-10 02:00:55 +0530

    post api added

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 app.get('/',(req, res) => {
     res.send('Hello world');
 });
 
-app.get('/api/cources',(req, res) => {
-    res.send([1,2,3,4]);
+app.get('/api/courses',(req, res) => {
+    res.status(200).send([1,2,3,4,5,6]);
 });
 
-app.listen(3000, () => {
-    console.log('listening on port 3000');
+app.get('/api/courses/:id',(req, res) => {
+    res.send(req.params.id);
+});
+
+var courses = [];
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
 })
