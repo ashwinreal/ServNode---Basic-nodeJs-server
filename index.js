411b3fbb345ac70d0bf3814b04649407356a37c6
Ashwin Sreekumar (assreeku) <assreeku@cisco.com>
2018-10-13 17:03:59 +0530

    added input validation, implement put,post,get

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const app = express();
+const Joi = require('joi');
+
 app.use(express.json());
 
+var courses = [];
 app.get('/',(req, res) => {
     res.send('Hello world');
 });
 
 app.get('/api/courses',(req, res) => {
-    res.status(200).send([1,2,3,4,5,6]);
+    res.status(200).send(courses);
 });
 
 app.get('/api/courses/:id',(req, res) => {
-    res.send(req.params.id);
+    let id = parseInt(req.params.id);
+    let course = courses.filter(c => c.id === id);
+    if(!course.length){
+        res.status(404).send('id not found');
+        return;
+    }
+    res.send(course);
 });
 
-var courses = [];
 app.post('/api/courses', (req, res) => {
+    const valRes = validateCourse(req.body);
+    if(valRes.error){
+        res.status(400).send('Input validation failed. '+valRes.error);
+        return;
+    }
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -23,6 +36,32 @@ app.post('/api/courses', (req, res) => {
     courses.push(course);
     res.send(course);
 });
+
+app.put('/api/courses/:id',(req, res) => {
+    const valRes = validateCourse(req.body);
+    if(valRes.error){
+        res.status(400).send('Input validation failed. '+valRes.error);
+        return;
+    }
+    let id = parseInt(req.params.id);
+    let course = courses.filter(c => c.id === id);
+    if(!course.length){
+        res.status(404).send('id not found');
+        return;
+    }
+    course = course[0];
+    course.name = req.body.name;
+    res.send(course);
+})
+
+function validateCourse(course){
+    const schema = Joi.object().keys({
+        name: Joi.string().min(3).required()
+    });
+
+    return Joi.validate(course, schema);
+}
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
