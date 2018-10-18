50ae4fd441f5816e395991ece117ec835ea7c9c5
Ashwin Sreekumar (assreeku) <assreeku@cisco.com>
2018-10-18 13:22:44 +0530

    move middlewares to a common folder

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(helmet());
+if(app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled');
+}
+app.use(require('./middleware/logger'));
 app.use('/api/courses', courses);
 app.use('/',home);
 
 console.log(config.get('name'));
 console.log(config.get('mail.host'));
 // console.log(config.get('mail.password'));
-if(app.get('env') === 'development'){
-    app.use(morgan('tiny'));
-    console.log('Morgan enabled');
-}
-// app.use(require('./logger'));
 
-const PORT = process.env.PORT || 3006;
+const PORT = process.env.PORT || 3007;
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 })
